f38de98387356a2100e4db8cd1764b41d25591f4
bigfaiber <greatfaiber@gmail.com>
2020-04-22 19:17:52 -0500

    custom error message for param

--- app/api/version1.rb
+++ app/api/version1.rb
@@ -1,16 +1,18 @@
 require_relative '../../config/application'
-require 'json'
+
 module MyApi
   class CinemaApiV1 < Grape::API
     version 'v1', :using => :path
     format :json
 
     resource :movies do
+      params do
+        requires :dow, type: {value: Integer, message: "type is invalid, must be an integer"}
+      end
       desc "Movies by dow"
       get '/by_day/:dow' do
-        {movies: Dow.find(dow: params[:dow].to_i).movies}
+          {movies: Dow.find(dow: params[:dow]).movies}
       end
-
     end
   end
 end
